f7404b3b2b25e6a8d6ebdba8f014b1fb0493c234
Pluto! <Rheddweb2007@gmail.com>
2025-07-01 01:38:57 -0500

    P320 Frame fix

--- src/mod.js
+++ src/mod.js
@@ -38,13 +38,12 @@ const fs = __importStar(require("fs"));
 const path = __importStar(require("path"));
 const ConfigTypes_1 = require("C:/snapshot/project/obj/models/enums/ConfigTypes");
 const Traders_1 = require("C:/snapshot/project/obj/models/enums/Traders");
-// WTT / Viper Item Imports
+// Your other imports...
 const WTTInstanceManager_1 = require("./WTTInstanceManager");
 const EpicsEdits_1 = require("./EpicsEdits");
 const CustomItemService_1 = require("./CustomItemService");
 const CustomAssortSchemeService_1 = require("./CustomAssortSchemeService");
 const CustomWeaponPresets_1 = require("./CustomWeaponPresets");
-// Trader Imports
 const References_1 = require("./Refs/References");
 const TraderTemplate_1 = require("./Trader/TraderTemplate");
 const Utils_1 = require("./Refs/Utils");
@@ -54,16 +53,13 @@ class EchoesOfTarkovMod {
     modName = "Echoes of Tarkov - Requisitions & hoser";
     version;
     debug = false;
-    // WTT-related Services
     Instance = new WTTInstanceManager_1.WTTInstanceManager();
     customItemService = new CustomItemService_1.CustomItemService();
     customAssortSchemeService = new CustomAssortSchemeService_1.CustomAssortSchemeService();
     customWeaponPresets = new CustomWeaponPresets_1.CustomWeaponPresets();
     epicItemClass = new EpicsEdits_1.epicItemClass();
-    // Trader-related Services
     ref = new References_1.References();
     preSptLoad(container) {
-        // WTT Initializations
         this.Instance.preSptLoad(container, this.modName);
         this.Instance.debug = this.debug;
         this.getVersionFromJson();
@@ -71,7 +67,6 @@ class EchoesOfTarkovMod {
         this.customAssortSchemeService.preSptLoad(this.Instance);
         this.customWeaponPresets.preSptLoad(this.Instance);
         this.epicItemClass.preSptLoad(this.Instance);
-        // Trader Initializations
         this.ref.preSptLoad(container);
         const ragfair = this.ref.configServer.getConfig(ConfigTypes_1.ConfigTypes.RAGFAIR);
         const traderConfig = this.ref.configServer.getConfig(ConfigTypes_1.ConfigTypes.TRADER);
@@ -79,12 +74,10 @@ class EchoesOfTarkovMod {
         const traderData = new TraderTemplate_1.TraderData(traderConfig, this.ref, traderUtils);
         traderData.registerProfileImage();
         traderData.setupTraderUpdateTime();
-        // Register hoser for Ragfair
         Traders_1.Traders[baseJson._id] = baseJson._id;
         ragfair.traders[baseJson._id] = true;
     }
     postDBLoad(container) {
-        // WTT Initializations
         this.Instance.postDBLoad(container);
         console.log(`\x1b[94m[Echoes of Tarkov] \x1b[93m Requisitions Loaded | Got something I'm supposed to deliver - your hands only.`);
         console.log(`\x1b[94m[Echoes of Tarkov] \x1b[93m Hoser Loaded        | Don’t ask for a discount. You want magic, you pay sorcerer prices.`);
@@ -92,7 +85,6 @@ class EchoesOfTarkovMod {
         this.customAssortSchemeService.postDBLoad();
         this.customWeaponPresets.postDBLoad();
         this.epicItemClass.postDBLoad();
-        // Trader Setup
         this.ref.postDBLoad(container);
         const traderConfig = this.ref.configServer.getConfig(ConfigTypes_1.ConfigTypes.TRADER);
         const traderUtils = new Utils_1.TraderUtils();
--- src/mod.ts
+++ src/mod.ts
@@ -10,14 +10,12 @@ import { ConfigTypes } from "@spt/models/enums/ConfigTypes";
 import { ITraderConfig } from "@spt/models/spt/config/ITraderConfig";
 import { Traders } from "@spt/models/enums/Traders";
 
-// WTT / Viper Item Imports
+// Your other imports...
 import { WTTInstanceManager } from "./WTTInstanceManager";
 import { epicItemClass } from "./EpicsEdits";
 import { CustomItemService } from "./CustomItemService";
 import { CustomAssortSchemeService } from "./CustomAssortSchemeService";
 import { CustomWeaponPresets } from "./CustomWeaponPresets";
-
-// Trader Imports
 import { References } from "./Refs/References";
 import { TraderData } from "./Trader/TraderTemplate";
 import { TraderUtils } from "./Refs/Utils";
@@ -29,18 +27,15 @@ class EchoesOfTarkovMod implements IPreSptLoadMod, IPostDBLoadMod {
 	private version: string;
 	private debug = false;
 
-	// WTT-related Services
 	private Instance: WTTInstanceManager = new WTTInstanceManager();
 	private customItemService: CustomItemService = new CustomItemService();
 	private customAssortSchemeService: CustomAssortSchemeService = new CustomAssortSchemeService();
 	private customWeaponPresets: CustomWeaponPresets = new CustomWeaponPresets();
 	private epicItemClass: epicItemClass = new epicItemClass();
 
-	// Trader-related Services
 	private ref: References = new References();
 
 	public preSptLoad(container: DependencyContainer): void {
-		// WTT Initializations
 		this.Instance.preSptLoad(container, this.modName);
 		this.Instance.debug = this.debug;
 		this.getVersionFromJson();
@@ -50,7 +45,6 @@ class EchoesOfTarkovMod implements IPreSptLoadMod, IPostDBLoadMod {
 		this.customWeaponPresets.preSptLoad(this.Instance);
 		this.epicItemClass.preSptLoad(this.Instance);
 
-		// Trader Initializations
 		this.ref.preSptLoad(container);
 		const ragfair = this.ref.configServer.getConfig(ConfigTypes.RAGFAIR);
 		const traderConfig: ITraderConfig = this.ref.configServer.getConfig<ITraderConfig>(ConfigTypes.TRADER);
@@ -60,24 +54,21 @@ class EchoesOfTarkovMod implements IPreSptLoadMod, IPostDBLoadMod {
 		traderData.registerProfileImage();
 		traderData.setupTraderUpdateTime();
 
-		// Register hoser for Ragfair
 		Traders[baseJson._id] = baseJson._id;
 		ragfair.traders[baseJson._id] = true;
 	}
 
 	public postDBLoad(container: DependencyContainer): void {
-		// WTT Initializations
 		this.Instance.postDBLoad(container);
 
 		console.log(`\x1b[94m[Echoes of Tarkov] \x1b[93m Requisitions Loaded | Got something I'm supposed to deliver - your hands only.`);
-				console.log(`\x1b[94m[Echoes of Tarkov] \x1b[93m Hoser Loaded        | Don’t ask for a discount. You want magic, you pay sorcerer prices.`);
+		console.log(`\x1b[94m[Echoes of Tarkov] \x1b[93m Hoser Loaded        | Don’t ask for a discount. You want magic, you pay sorcerer prices.`);
 
 		this.customItemService.postDBLoad();
 		this.customAssortSchemeService.postDBLoad();
 		this.customWeaponPresets.postDBLoad();
 		this.epicItemClass.postDBLoad();
 
-		// Trader Setup
 		this.ref.postDBLoad(container);
 		const traderConfig: ITraderConfig = this.ref.configServer.getConfig<ITraderConfig>(ConfigTypes.TRADER);
 		const traderUtils = new TraderUtils();
@@ -94,7 +85,6 @@ class EchoesOfTarkovMod implements IPreSptLoadMod, IPostDBLoadMod {
 			baseJson.location,
 			"Hoser is a profit-driven ex-Canadian combat engineer turned black market mod dealer, selling high-end gun parts to anyone with the cash—no loyalties, no questions."
 		);
-
 	}
 
 	private getVersionFromJson(): void {
